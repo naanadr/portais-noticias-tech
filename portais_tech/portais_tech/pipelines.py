2305071f3f81e1b0009a20453babc94edd98897e
FernandaSzAdr <fsandrade25@gmail.com>
2020-02-21 19:15:38 -0300

    Adiciona as configurações do mongo no pipelines.py

--- portais_tech/portais_tech/pipelines.py
+++ portais_tech/portais_tech/pipelines.py
@@ -1,11 +1,14 @@
-# -*- coding: utf-8 -*-
+import pymongo
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.conf import settings
 
 
 class PortaisTechPipeline(object):
-    def process_item(self, item, spider):
-        return item
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]

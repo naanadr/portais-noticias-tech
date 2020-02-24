5bef594d9f53ef9717bd8236d3ce2a8bae724ea3
FernandaSzAdr <fsandrade25@gmail.com>
2020-02-24 12:58:21 -0300

    Atualiza modo de realização de conexão com o mongodb

--- portais_tech/portais_tech/pipelines.py
+++ portais_tech/portais_tech/pipelines.py
@@ -1,27 +1,40 @@
 import pymongo
 
-from scrapy.utils.project import get_project_settings
 from scrapy.exceptions import DropItem
 
 
 class MongoPipeline(object):
 
-    def __init__(self):
-        settings = get_project_settings()
-        connection = pymongo.MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
+    collection_name = 'tech'
+
+    def __init__(self, mongo_server, mongo_port, mongo_db):
+        self.mongo_server = mongo_server
+        self.mongo_port = mongo_port
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_server=crawler.settings.get('MONGO_SERVER'),
+            mongo_port=crawler.settings.get('MONGO_PORT'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
         )
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_server, self.mongo_port)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
 
     def process_item(self, item, spider):
+        collection = self.db[self.collection_name]
         for data in item:
             if not data:
                 raise DropItem(f'Item perdido {data}!')
         else:
-            if item.get('url') in self.collection.distinct('url'):
+            if item.get('url') in collection.distinct('url'):
                 raise DropItem(f'Item Duplicado {item.get("url")}')
             else:
-                self.collection.insert(dict(item))
+                collection.insert(dict(item))
         return item
--- portais_tech/portais_tech/settings.py
+++ portais_tech/portais_tech/settings.py
@@ -52,33 +52,10 @@ ROBOTSTXT_OBEY = True
 #}
 
 # Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'portais_tech.pipelines.MongoPipeline': 300,
+   'portais_tech.pipelines.MongoPipeline': 500,
 }
 
-MONGODB_SERVER = "db"
-MONGODB_PORT = 27017
-MONGODB_DB = "portaisnoticias"
-MONGODB_COLLECTION = "tech"
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+MONGO_PORT = 27017
+MONGO_SERVER = "db"
+MONGO_DATABASE = "portaisnoticias"
